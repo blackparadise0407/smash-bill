07dd4b3767d6baaaff9072360c0e114e1c7ca4ee
blackparadise0407 <blackparadise0407@gmail.com>
2026-06-04 15:22:06 +0700

    style: update minimum height class in HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import IdentityGate from "@/components/IdentityGate";
 
 export default function HomePage() {
   return (
-    <main className="mx-auto flex justify-center min-h-screen w-full max-w-5xl flex-col px-5 py-8 md:px-8">
+    <main className="mx-auto flex justify-center min-h-svh w-full max-w-5xl flex-col px-5 py-8 md:px-8">
       <IdentityGate />
     </main>
   );
